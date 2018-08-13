9a233e2855551566687c0f313a2697eb68703576
Kai Wood <kwood@kwd.io>
2018-08-13 12:05:48 +0200

    Document method

--- lib/pnut/posts.rb
+++ lib/pnut/posts.rb
@@ -1,4 +1,7 @@
 module Posts
+  # <em>GET /posts/streams/global</em>
+  #
+  # A stream of all users' public posts.
   def global(**args)
     self.request("/posts/streams/global", params: args)
   end
